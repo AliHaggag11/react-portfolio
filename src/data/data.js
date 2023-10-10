4509d4ecbbca0de6ad5bd1c507ff246e2b39ddfe
AliHaggag11 <Ali7aggag@gmail.com>
2023-10-10 19:54:17 +0300

    first

---
+++ src/data/data.js
@@ -0,0 +1,30 @@
+import Upscale from '../assets/projects/upscale.jpeg'
+import Blog from '../assets/projects/blog.jpeg'
+import Dataf from '../assets/projects/datafinance.jpeg'
+
+export const data=[
+    {
+        id:1,
+        name:"Upscale Agency Website",
+        image:Upscale,
+        github:"https://upscale-da.netlify.app/",
+        live:"https://upscale-da.netlify.app/",
+    },
+    {
+        id:2,
+        name:"React Sanity Blog",
+        image:Blog,
+        github:"https://github.com/AliHaggag11/blog-nextjs-sanity",
+        live:"https://techpanda.vercel.app/",
+    },
+    {
+        id:3,
+        name:"Data Finance LP",
+        image:Dataf,
+        github:"https://github.com/AliHaggag11/Data-Finance-Landing-Page",
+        live:"data-finance-lp.vercel.app",
+    },
+    
+
+
+]
