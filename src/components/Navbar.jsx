8f8448198fe5be129905bb46cc15477bd55bebaf
AliHaggag11 <Ali7aggag@gmail.com>
2023-11-12 21:53:10 +0200

    mail fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -115,12 +115,13 @@ const Navbar = () => {
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-zinc-900'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='ali7aggag@gmail.com'
-            >
-              Email <HiOutlineMail size={30} />
-            </a>
+          <a
+  className='flex justify-between items-center w-full text-gray-300'
+  href='mailto:ali7aggag@gmail.com'
+>
+  <HiOutlineMail size={30} />
+</a>
+
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
