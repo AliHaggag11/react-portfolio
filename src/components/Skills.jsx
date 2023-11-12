f32cb35685b59c9ea962153866628d3fe4d81086
AliHaggag11 <Ali7aggag@gmail.com>
2023-11-12 21:32:24 +0200

    Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,11 +12,11 @@ import Mongo from '../assets/mongo.png';
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+    <div name='skills' className='w-full h-screen bg-[#1e1e1e] text-gray-300'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
-              <p className='text-4xl font-bold inline border-b-4 border-orange-600 '>Skills</p>
+              <p className='text-4xl font-bold inline border-b-4 border-purple-600 '>Skills</p>
               <p className='py-4'>// These are the technologies I excel at</p>
           </div>
 
