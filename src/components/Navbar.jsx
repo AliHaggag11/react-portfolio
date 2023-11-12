a22972b7fa4e8244d6a92e0f6c5f5516d9455b87
AliHaggag11 <Ali7aggag@gmail.com>
2023-11-12 22:12:00 +0200

    Mail And Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -117,7 +117,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-zinc-900'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='ali7aggag@gmail.com'
+              href='mailto:ali7aggag@gmail.com'
             >
               Email <HiOutlineMail size={30} />
             </a>
