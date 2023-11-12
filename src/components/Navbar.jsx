af2a77c26293cbdb12d59284661e501850150710
AliHaggag11 <Ali7aggag@gmail.com>
2023-11-12 22:02:02 +0200

    Nav Co

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#1e1e1e] text-purple-600'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#1e1e1e] text-slate-300'>
       <div>
         <img src={Logo} alt='Logo Image' style={{ width: '150px' }} />
       </div>
