54c7ba356bd34fde0bf2f293f5003efa9fbc8eaa
Ali Haggag <88595616+AliHaggag11@users.noreply.github.com>
2023-11-12 21:37:56 +0200

    Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,21 +11,19 @@ const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-orange-600 text-3xl' >Hi, my name is</p>
+        <p className='text-purple-600 text-3xl' >Hi, I'm </p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
-          ALI HAGGAG
+          Ali Haggag
         </h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
           I'm a Full Stack Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a full-stack developer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
-          building responsive full-stack web applications.
+        I'm not just your average full-stack developer – I'm a digital architect crafting extraordinary online experiences. While I dabble in design, my real forte lies in building responsive and dynamic full-stack web applications. Let's not just navigate the digital world; let's conquer it together. Ready to transform your online presence into something truly exceptional?
         </p>
         <div>
           <button 
-            className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-cyan-600 hover:border-cyan-600'
+            className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-purple-600 hover:border-purple-600'
             onClick={handleButtonClick}
           >
             View Work
