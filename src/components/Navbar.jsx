25016e569fe9b0cf9d1bda7c58efbd9710c3b14c
AliHaggag11 <Ali7aggag@gmail.com>
2023-11-12 21:28:11 +0200

    Nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#1e1e1e] text-purple-600'>
       <div>
         <img src={Logo} alt='Logo Image' style={{ width: '150px' }} />
       </div>
@@ -61,7 +61,7 @@ const Navbar = () => {
         className={
           !nav
             ? 'hidden'
-            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#1e1e1e] flex flex-col justify-center items-center'
         }
       >
         <li className='py-6 text-4xl'>
